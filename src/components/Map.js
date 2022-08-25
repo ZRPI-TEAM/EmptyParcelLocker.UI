f0dda3fde914e9ab3a160fd049e3cbf22f94d1c8
Konrad Izdebski <konradizdebski1011@gmail.com>
2022-08-25 14:51:02 +0200

    Add marker template to project

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,18 +4,28 @@ import './Map.css';
 
 // Importing all Leaflet stuff
 
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Map = () => {
+
+    const position = [51.505, -0.09]
+
     return ( 
         <div className="map">
             <MapContainer id='mapContainer' center={[51.505, -0.09]} 
             zoom={13} 
-            scrollWheelZoom={false}>
+            scrollWheelZoom={true}>
             <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+
+            <Marker position={position}>
+                <Popup>
+                    To jest paczkomat
+                    <br /> Ma nazwę id i wogle 
+                </Popup>
+            </Marker>
             </MapContainer>
         </div>
      );
