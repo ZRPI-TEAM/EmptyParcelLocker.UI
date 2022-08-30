9c9d84771d48aaff9bbbf808de8dbb337a6cb1ec
Konrad Izdebski <konradizdebski1011@gmail.com>
2022-08-30 13:38:26 +0200

    CSS class fix

--- src/components/LockerOptionsActive.js
+++ src/components/LockerOptionsActive.js
@@ -9,10 +9,12 @@ const LockerOptionsActive = (props) => {
 
     return ( 
         <div className="locker-options">
-            <h1 className='locker-options__header'>Rzeczy paczkomatowe</h1>
-            <button 
-                onClick={handleDisplaySwitch}
-                className='locker-options__button--showState'>Wróć</button>
+            <div className="locker-options__display">
+                <h1 className='locker-options__header'>Rzeczy paczkomatowe</h1>
+                <button 
+                    onClick={handleDisplaySwitch}
+                    className='locker-options__button--showState'>Wróć</button>
+            </div>
         </div>
      );
 }
