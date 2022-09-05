d60f6fb5c42f365f96c89704faa18544baecb6b0
Konrad Izdebski <konradizdebski1011@gmail.com>
2022-09-05 10:31:47 +0200

    Added types of lockers to Active Locker component

--- src/components/LockerOptionsActive.js
+++ src/components/LockerOptionsActive.js
@@ -11,9 +11,26 @@ const LockerOptionsActive = (props) => {
         <div className="locker-options">
             <div className="locker-options__display">
                 <h1 className='locker-options__header'>Rzeczy paczkomatowe</h1>
+
+                <div className="locker-options__available">
+                    <h2 className="locker-options__available--header">
+                        Dostępne skrytki:
+                    </h2>
+                    <h3 className="locker-options__available--small">
+                        Małe:
+                    </h3>
+                    <h3 className="locker-options__available--medium">
+                        Średnie:
+                    </h3>
+                    <h3 className="locker-options__available--large">
+                        Duże:
+                    </h3>
+                </div>
+
                 <button 
                     onClick={handleDisplaySwitch}
-                    className='locker-options__button--showState'>Wróć</button>
+                    className='locker-options__button--showState'>Wróć
+                </button>
             </div>
         </div>
      );
